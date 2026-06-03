6f80a78e224a01c773b19da40f00d31ec5a8b78d
PadjokeJ <jonatan@padjokej.dev>
2026-06-03 18:07:03 +0200

    feat: exam 2024 start

--- Extras/cpu.v
+++ Extras/cpu.v
@@ -31,7 +31,8 @@ module mux #(
 endmodule
 
 module registerFile (
-    input clk, input [4:0] reg1, input [4:0] reg2, input [4:0] wreg, input [31:0] wdata, input regWrite,
+    input clk,
+    input [4:0] reg1, input [4:0] reg2, input [4:0] wreg, input [31:0] wdata, input regWrite,
     output reg [31:0] dat1, output reg [31:0] dat2
 );
     reg [31:0] file [0:31];
@@ -52,10 +53,33 @@ module immGen (
         case (instruction[6:0])
             //7'h03: generated <= ; // I-type
             7'h13: generated <= {{21{instruction[31]}},instruction[30:20]}; // I-type
-            //7'h23: generated <= ; // S-type
-            //7'h33: generated <= ; // R-type
-            //7'h37: generated <= ; // U-type
-            //7'h63: generated <= {,1'b0}; // B-type
+            7'h23: generated <= {{{21{instruction[31]}},instruction[31:25]},instruction[11:7]}; // S-type
+            7'h33:; // R-type
+            7'h37: generated <= {instruction[31:12],12'b0}; // U-type
+            7'h63: generated <= {{instruction[12],instruction[7]},{instruction[30:25],instruction[11:8]},1'b0}; // B-type
         endcase
+    end
+endmodule
+
+module ALU (
+    input [31:0] dat1, input [31:0] dat2, input ALUControl,
+    output reg zero, output reg [31:0] res
+);
+    always @(*) begin
+        
+    end
+endmodule
+
+module cpu;
+    reg clk;
+    
+    reg [31:0] instruction;
+
+    reg [31:0] wdata;
+
+    //registerFile rf(.clk(clk), .reg1(instruction[19:15]), .reg2(instruction[24:20]), .wreg(instruction[11:7]), .wdata(wdata), .regWrite(cRegWrite), .dat1(), .dat2());
+
+    always begin
+        #5 clk = ~clk;
     end
 endmodule
